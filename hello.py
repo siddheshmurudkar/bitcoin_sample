351d096a7166d78dc1e8f0e861b0c4006a39404e
siddheshmurudkar <siddhesh@curiositygym.com>
2018-04-18 17:26:14 +0530

    added ifttt code

--- hello.py
+++ hello.py
@@ -1,17 +1,20 @@
 from flask import Response, Flask, current_app as app
 import os
+import requests
+import random
+import string
 app = Flask(__name__,static_url_path='/static')
 
-
+'''
 @app.route("/")
 def hello():
    return app.send_static_file('index.html')   
-
+'''
    
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
-
+'''
 @app.route("/newPage")
 def newPage():
    return "This is the new Page"
@@ -19,6 +22,35 @@ def newPage():
 @app.route("/add/<a>/<b>")
 def parameters(a,b):
    return str(int(a)+int(b))
+'''
+
+
+def email_alert(first, second, key):
+    report = {}
+    report["value1"] = first
+    report["value2"] = second
+    report["value3"] = third
+    requests.post("https://maker.ifttt.com/trigger/bitcoin/with/key/"+key, data=report)    
+
+def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+@app.route("/device1")
+def device1():
+   user_emailID = os.environget('device1')
+   secret_code = random_generator()
+   IFTTT_KEY = os.environ.get('IFTTT_KEY')
+   email_alert(user_emailID, secret_code, IFTTT_KEY)
+   #return app.send_static_file('index.html')
+
+@app.route("/device2")
+def device2():
+   user_emailID = os.environget('device1')
+   secret_code = random_generator()
+   IFTTT_KEY = os.environ.get('IFTTT_KEY')
+   email_alert(user_emailID, secret_code, IFTTT_KEY)
+   #return app.send_static_file('index.html')   
+
 
 
 if __name__ == "__main__":
